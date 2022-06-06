497b0ac45e9702d0ea299238e4d776fa90a7dda6
Alex Huddell <48715797+ahuddell@users.noreply.github.com>
2022-06-06 13:58:29 -0400

    added more preliminary data analysis

--- yield_NDVI_MD_data.R
+++ yield_NDVI_MD_data.R
@@ -1,6 +1,8 @@
 library(here)
 library(tidyverse)
 library(lubridate)
+library(sf)
+
 
 dat<-read_csv(here('data','RS_biomass_sampling_AH.csv'))
 
@@ -9,7 +11,6 @@ names(dat)
 
 #to properly drop NAs with hardcoded 'na" values
 dat$Dry_BM_kG_ha<-if_else(dat$Dry_BM_kG_ha=='na', 'NA', dat$Dry_BM_kG_ha) 
-as.numeric(dat$Dry_BM_kG_ha)
 
 dat<-dat %>%
      mutate(biomass_sample_date=mdy(Field_sampling_date),
@@ -21,26 +22,23 @@ dat<-dat %>%
              Latitude,Longitude, Sentinel2_Date,NDVI_8A,
              Sentinel2_Date2,NDVI_8A2
              ) %>%
-     mutate(sample_year=year(biomass_sample_date),
-             sample_month=month(biomass_sample_date)
+     mutate(sample_year=(year(biomass_sample_date)),
+             sample_month=(month(biomass_sample_date, label=T))
              )
 dat  
+dat$sample_month
 
-unique(dat$loc_ID) #seems like there are 390 unique biomass points with precise 
+# biomass data exploration ------------------------------------------------
+
+unique(dat$loc_ID) #there are 390 unique biomass points with precise 
                   #GPS coordinates 
 biomass_dups<-dat %>% 
   group_by(loc_ID) %>% 
   tally() %>%
   filter(n>1) 
-biomass_dups #looks like 83 points have 2 biomass points
-unique(biomass_dups$loc_ID)
+biomass_dups #82 points have 2 biomass points
 
 #plot biomass 
-ggplot(dat, aes(loc_ID, biomass_kg_ha, col=as.factor(sample_year))) +
-  geom_point()+
-  theme(axis.text.x=element_text(angle=90,hjust=1)) 
-
-
 dat %>% 
   filter(loc_ID %in% biomass_dups$loc_ID) %>% 
   ggplot(aes(loc_ID, biomass_kg_ha, col=as.factor(sample_year))) +
@@ -48,13 +46,49 @@ dat %>%
   ggtitle('Biomass samples exact same place across 2 years')+
   theme(axis.text.x=element_text(angle=90,hjust=1)) 
 
+#look at biomass sampling dates
+dat2 %>%
+  group_by(biomass_sample_month=month(biomass_sample_date, label=T)) %>%
+  ggplot(aes(x=biomass_sample_month)) +
+  geom_bar() +
+  ggtitle('Distribution of biomass observations')
+
+# NDVI data exploration ---------------------------------------------------
+
 
 #drop rows without NDVI
 dat2<-dat %>%
   drop_na(NDVI_8A) 
 
-unique(dat2$loc_ID) #seems like there are 190 paired biomass/NDVI points with precise 
+unique(dat2$loc_ID) #seems like there are 195 paired biomass/NDVI points with precise 
 #GPS coordinates 
 
+NDVI_dups<-dat2 %>% 
+  group_by(loc_ID) %>% 
+  tally() %>%
+  filter(n>1) 
+NDVI_dups #25 points have 2 biomass points
 
+dat2
+
+#look at NDVI dates
+dat2 %>%
+  group_by(NDVI_date=month(mdy(Sentinel2_Date), label=T)) %>%
+ggplot(aes(x=NDVI_date)) +
+  geom_bar() +
+  ggtitle('Distribution of NDVI observations')
+
+
+# climate data ------------------------------------------------------------
+
+
+
+
+# modeling ----------------------------------------------------------------
+
+
+
+#distributions of NDVI and biomass_kg_ha
+hist(dat2$NDVI_8A)
+hist(dat$biomass_kg_ha)
 
