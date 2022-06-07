0d0bdddd6f26bc81655c99bba07a71bdc30f1fb8
Alex Huddell <48715797+ahuddell@users.noreply.github.com>
2022-06-07 09:43:20 -0400

    explored second observations of NDVI but we can ignore them since they don't add any useful data points

--- yield_NDVI_MD_data.R
+++ yield_NDVI_MD_data.R
@@ -2,6 +2,8 @@ library(here)
 library(tidyverse)
 library(lubridate)
 library(sf)
+library(httr)
+library(readr)
 
 
 dat<-read_csv(here('data','RS_biomass_sampling_AH.csv'))
@@ -23,7 +25,9 @@ dat<-dat %>%
              Sentinel2_Date2,NDVI_8A2
              ) %>%
      mutate(sample_year=(year(biomass_sample_date)),
-             sample_month=(month(biomass_sample_date, label=T))
+            sample_month=(month(biomass_sample_date, label=T)),
+            lat_short=round(dat$Latitude,1),
+            long_short=round(dat$Longitude,1)
              )
 dat  
 dat$sample_month
@@ -55,6 +59,30 @@ dat2 %>%
 
 # NDVI data exploration ---------------------------------------------------
 
+#########################
+#explore the second NDVI observations to see if we would ever want to use them
+
+#check whether any of the Sentinel2_Date2 come before Sentinel2_Date
+dat %>%
+  filter(mdy(Sentinel2_Date2)<mdy(Sentinel2_Date))
+#no--they all come after
+
+#are there any cases with only a second NDVI observation but not first?
+dat %>%
+  filter(is.na(NDVI_8A) & !is.na(NDVI_8A2))
+#no
+
+dat %>%
+  group_by(NDVI_date2=month(mdy(Sentinel2_Date2), label=T)) %>%
+  ggplot(aes(x=NDVI_date2)) +
+  geom_bar() +
+  ggtitle('Distribution of later NDVI observations')
+
+#the observations are either in Jan or May, and most rows are missing
+############################
+
+#explore the earlier NDVI observations since we don't need to use any of the 
+#second observations
 
 #drop rows without NDVI
 dat2<-dat %>%
@@ -76,13 +104,34 @@ dat2 %>%
   group_by(NDVI_date=month(mdy(Sentinel2_Date), label=T)) %>%
 ggplot(aes(x=NDVI_date)) +
   geom_bar() +
-  ggtitle('Distribution of NDVI observations')
+  ggtitle('Distribution of NDVI early observations')
+
 
 
 # climate data ------------------------------------------------------------
 
+max(dat$biomass_sample_date)
+lat=unname(dat$Latitude[1])
+lat
 
 
+
+## 2. Importing data from weather API
+weather <- GET(
+  url = 'https://api.precisionsustainableag.org/weather/daily?', # if you want hourly, replace 'daily' with 'hourly' in the url
+  query = list(
+    lat = 39.02434,
+    lon = -76.93934,
+    start = '2015-01-01',
+    end = '2022-05-01',
+    output='csv',
+    options = ''
+  )
+)
+weather_parsed <- readr::read_csv(
+  content(weather, as = "text"))
+
+weather_parsed
 
 # modeling ----------------------------------------------------------------
 
