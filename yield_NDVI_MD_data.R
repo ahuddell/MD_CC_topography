4a5d4608444ed022ae8218e9bb0fafbc3132c08f
Alex Huddell <48715797+ahuddell@users.noreply.github.com>
2022-06-10 15:43:21 -0400

    added biomass histogram with 1900 NDVI saturation threshold

--- yield_NDVI_MD_data.R
+++ yield_NDVI_MD_data.R
@@ -11,6 +11,7 @@ dat<-read_csv(here('data','RS_biomass_sampling_AH.csv'))
 #select most relevant columns 
 names(dat)
 
+
 #to properly drop NAs with hardcoded 'na" values
 dat$Dry_BM_kG_ha<-if_else(dat$Dry_BM_kG_ha=='na', 'NA', dat$Dry_BM_kG_ha) 
 
@@ -26,13 +27,25 @@ dat<-dat %>%
              ) %>%
      mutate(sample_year=(year(biomass_sample_date)),
             sample_month=(month(biomass_sample_date, label=T)),
-            lat_short=round(dat$Latitude,1),
-            long_short=round(dat$Longitude,1)
+            lat_short=round(Latitude,1),
+            long_short=round(Longitude,1)
              )
 dat  
 dat$sample_month
 
 # biomass data exploration ------------------------------------------------
+
+ggplot(dat, aes(biomass_kg_ha))+
+  geom_histogram()+
+  theme_minimal()+
+  xlab('Shoot biomass (kg/ha)')+
+  geom_vline(xintercept=1900, col='red')+
+  ggtitle("Rye yield data from Allison's data")
+
+dat_high<-filter(dat,biomass_kg_ha>1900)
+
+nrow(dat_high)
+nrow(dat_high)/nrow(dat)*100
 
 unique(dat$loc_ID) #there are 390 unique biomass points with precise 
                   #GPS coordinates 
@@ -86,8 +99,14 @@ dat %>%
 
 #drop rows without NDVI
 dat2<-dat %>%
-  drop_na(NDVI_8A) 
+  drop_na(NDVI_8A) %>%
+  mutate(NDVI_date=mdy(Sentinel2_Date),
+         NDVI_julian_day=yday(NDVI_date),
+         biomass_month=month(biomass_sample_date, label=T),
+         biomass_julian_day=yday(biomass_sample_date)
+         )
 
+dat2
 unique(dat2$loc_ID) #seems like there are 195 paired biomass/NDVI points with precise 
 #GPS coordinates 
 
@@ -95,18 +114,30 @@ NDVI_dups<-dat2 %>%
   group_by(loc_ID) %>% 
   tally() %>%
   filter(n>1) 
-NDVI_dups #25 points have 2 biomass points
+NDVI_dups #25 points have 2 or 3 biomass points
 
 dat2
 
 #look at NDVI dates
 dat2 %>%
-  group_by(NDVI_date=month(mdy(Sentinel2_Date), label=T)) %>%
-ggplot(aes(x=NDVI_date)) +
+  group_by(NDVI_month) %>%
+ggplot(aes(x=NDVI_month)) +
   geom_bar() +
-  ggtitle('Distribution of NDVI early observations')
+  ggtitle('Distribution of NDVI observations')
 
 
+#make a plot of early season NDVI observation y1 and biomass sample y2
+ggplot(dat2, aes(x=loc_ID, 
+                 ymin=as_date(as.numeric(NDVI_julian_day), origin = lubridate::origin),
+                 ymax=as_date(as.numeric(biomass_julian_day), origin = lubridate::origin)
+                )
+       )+
+  geom_linerange(size=4) +
+  coord_flip()+
+  ggtitle("Time interval between NDVI date and biomass sample date") +
+  theme_minimal() +
+  scale_y_date(date_labels = "%b")
+  
 
 # climate data ------------------------------------------------------------
 
